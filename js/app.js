f4f850f02b0a234e2c9b3b8740ddb7da806c8b1b
uuii13524-afk <uuii13524@gmail.com>
2026-05-13 15:26:22 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 window.onload = async () => {
   await Registry.load();
-
   Router.init();
   UI.init();
+  
+  // 初期ツールを設定（hashがない場合はjsonをデフォルトに）
+  if (!State.currentTool) {
+    State.currentTool = "json";
+  }
+  
+  UI.renderNav();
   UI.render();
 
-  // PWA登録
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("service-worker.js");
   }
